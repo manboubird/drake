5b609d5e49ae790a4275386853c8bac947741032
Toshiaki Toyama <ttoyama@knownstylenolife.com>
2014-09-22 19:23:40 +0900

    Tweak build file to build against CDH 2.0.0-mr1-cdh4.5.0 with drake v1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
-(defproject factual/drake "1.0.0"
+(def HADOOP-CDH-MR1-VERSION  (str "2.0.0-mr1-cdh4.5.0"))
+(def HADOOP-CDH-VERSION  (str "2.0.0-cdh4.5.0"))
+(defproject factual/drake "1.0.0-mr1-cdh4.5.0"
   :description "Drake: the data processing workflow tool (a.k.a. 'make for data')"
   :url "https://github.com/Factual/drake"
   :license {:name "Eclipse Public License"
@@ -7,6 +9,7 @@
         :url "https://github.com/Factual/drake"}
   :signing {:gpg-key "1402451C"}
   :deploy-repositories [["clojars" {:creds :gpg}]]
+  :repositories {"cdh.releases.repo" "https://repository.cloudera.com/artifactory/cloudera-repos/"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.memoize "0.5.6"]
                  [factual/drake-interface "0.0.1"]
@@ -27,7 +30,10 @@
                  [factual/sosueme "0.0.15"]
                  [factual/c4 "0.2.1"]
                  [hdfs-clj "0.1.3"]    ;; for HDFS support
-                 [org.apache.hadoop/hadoop-core "0.20.2"]
+                 ;; you may need to change this to be compatible with your cluster
+                 [org.apache.hadoop/hadoop-core ~HADOOP-CDH-MR1-VERSION]
+                 [org.apache.hadoop/hadoop-hdfs ~HADOOP-CDH-VERSION]
+                 [org.apache.hadoop/hadoop-common ~HADOOP-CDH-VERSION]
                  [clj-aws-s3 "0.3.10" :exclusions [joda-time]]    ;; for AWS S3 support
                  ;; for plugins
                  [com.cemerick/pomegranate "0.2.0"]]
@@ -35,5 +41,4 @@
                    :default      (complement :regression)
                    :all          (constantly true)}
   :main drake.core
-  :uberjar-name "drake.jar"
   :aot :all)
